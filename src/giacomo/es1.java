72b4e902c1d39def52f56017b95f5abc0897d7cc
Giacomo <Giacomo@DESKTOP-HB4D7FE>
2023-11-12 18:41:20 +0100

    ciao

---
+++ src/giacomo/es1.java
@@ -0,0 +1,11 @@
+package giacomo;
+
+public class es1 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 9;
+System.out.println(a+5);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Giacomo
+ *
+ */
+module giacomo {
+}
