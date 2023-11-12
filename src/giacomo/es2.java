7f867fe1e120024e09075fa8cc857de37b191a2a
Giacomo <Giacomo@DESKTOP-HB4D7FE>
2023-11-12 19:05:31 +0100

    w

---
+++ src/giacomo/es2.java
@@ -0,0 +1,37 @@
+package giacomo;
+
+public class es2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] matrice = new int[10][10];
+		int cont = 0;
+		int a = 0;
+		//riempo la matrice a mano e verifico se il numero zaero è dentro la matrice
+		for (int i=0; i<matrice.length; i++)
+		{
+			for (int j=0; j<matrice.length; j++)
+			{
+				matrice[i][j]= (int) (Math.random()*10);
+				if(a == matrice[i][j])
+				{
+					cont += 2;
+				}
+			}
+		}
+		
+		//stampo la matrice
+		for (int i=0; i<matrice.length; i++)
+		{
+			for (int j=0; j<matrice.length; j++)
+			{
+				System.out.print(matrice[i][j]+"\t");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println("Il numero " + a + " si trova " + cont + " volte all'interno della matrice");
+
+	}
+
+}
